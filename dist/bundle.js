e6c8e03d332db37eae607e70af255d77b384afa1
unknown <dannythanhphan@gmail.com>
2020-12-07 20:18:46 -0800

    Add Graph and display data

--- dist/bundle.js
+++ dist/bundle.js
@@ -11192,18 +11192,6 @@ module.exports = JSON.parse("{\"fighting\":[{\"name\":\"ARMS\",\"earnings\":1824
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _public_check_json__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../public/check.json */ "./public/check.json");
 var _public_check_json__WEBPACK_IMPORTED_MODULE_0___namespace = /*#__PURE__*/__webpack_require__.t(/*! ../public/check.json */ "./public/check.json", 1);
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
-
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(n); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
-
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
 __webpack_require__(/*! babel-polyfill */ "./node_modules/babel-polyfill/lib/index.js");
 
 
@@ -11217,21 +11205,53 @@ document.addEventListener("DOMContentLoaded", function () {
       width = 460 - margin.left - margin.right,
       height = 400 - margin.top - margin.bottom;
 
-  function getHighestEarning(data) {
+  var getHighestEarning = function getHighestEarning(data) {
     var max = 0;
 
-    var copy = _toConsumableArray(data);
-
-    console.log(copy);
-
-    for (var i = 0; i < copy.length; i++) {
-      if (copy[i].earnings > max) max = copy[i].earnings;
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].earnings > max) max = data[i].earnings;
     }
 
     return max;
-  }
+  };
 
   var svg = d3.select("#dataset").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var updateChart = function updateChart(data) {
+    var dataGames = d3.nest().key(function (d) {
+      console.log(d);
+      return d["Year"];
+    }).entries(data.fighting);
+    var filteredData = dataGames[0]["values"];
+    var xMax = filteredData.length;
+    var x = d3.scaleLinear().domain([0, xMax]).range([0, width]);
+    svg.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x)); //Add x-axis label:
+
+    svg.append("text").attr("transform", "translate(" + width / 2 + " ," + (height + margin.top + 30) + ")").style("text-anchor", "middle").text("Adult Literacy");
+    var yMax = getHighestEarning(filteredData);
+    var y = d3.scaleLinear().domain([0, yMax]).range([height, 0]);
+    svg.append("g").call(d3.axisLeft(y)); //Add y-axis label:
+
+    svg.append("text").attr("transform", "rotate(-90)").attr("y", -40).attr("x", 0 - height / 2).style("text-anchor", "middle").text("Poverty Rate"); // Color scale: input a province name, output a color
+
+    var color = d3.scaleOrdinal().domain(["Balochistan", "Federal Capital Territory", "Khyber Pakhtunkhwa", "Punjab", "Sindh"]).range(["#440154ff", "#21908dff", "#fde725ff", "#129490", "#CE1483"]); // JOIN data to elements.
+
+    var circles = svg.selectAll("circle").data(filteredData, function (d) {
+      return d["name"];
+    });
+    var xAxis = 0; // ENTER new elements present in new data.
+
+    circles.enter().append("circle").attr("fill", function (d) {
+      return color(d["name"]);
+    }).attr("cy", function (d) {
+      return y(d["earnings"]);
+    }).attr("cx", function (d) {
+      xAxis++;
+      return x(xAxis);
+    }).attr("r", 5);
+  };
+
+  updateChart(_public_check_json__WEBPACK_IMPORTED_MODULE_0__);
 });
 
 /***/ })
--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 require("babel-polyfill");
-import data from '../public/check.json';
+import games from '../public/check.json';
 
 document.addEventListener("DOMContentLoaded", () => {
     let margin = { top: 10, right: 30, bottom: 50, left: 60 },
         width = 460 - margin.left - margin.right,
         height = 400 - margin.top - margin.bottom;
 
-    function getHighestEarning(data) {
+    const getHighestEarning = (data) => {
         let max = 0;
-        let copy = [...data];
-        console.log(copy)
-        for (let i = 0; i < copy.length; i++) {
-            if (copy[i].earnings > max) max = copy[i].earnings;
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].earnings > max) max = data[i].earnings;
         }
-
         return max;
     }
 
@@ -24,5 +21,84 @@ document.addEventListener("DOMContentLoaded", () => {
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    
+
+
+    const updateChart = (data) => {
+        let dataGames = d3
+            .nest()
+            .key(function (d) {
+                console.log(d)
+                return d["Year"];
+            })
+            .entries(data.fighting);
+
+        let filteredData = dataGames[0]["values"];
+
+        let xMax = filteredData.length;
+        let x = d3.scaleLinear().domain([0, xMax]).range([0, width]);
+        svg
+            .append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+        
+        //Add x-axis label:
+        svg
+            .append("text")
+            .attr(
+            "transform",
+            "translate(" + width / 2 + " ," + (height + margin.top + 30) + ")"
+            )
+            .style("text-anchor", "middle")
+            .text("Adult Literacy");
+
+        
+        let yMax = getHighestEarning(filteredData);
+
+        let y = d3.scaleLinear().domain([0, yMax]).range([height, 0]);
+        svg.append("g").call(d3.axisLeft(y));
+        
+        //Add y-axis label:
+        svg
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", -40)
+            .attr("x", 0 - height / 2)
+            .style("text-anchor", "middle")
+            .text("Poverty Rate");
+        
+        // Color scale: input a province name, output a color
+        let color = d3
+            .scaleOrdinal()
+            .domain([
+            "Balochistan",
+            "Federal Capital Territory",
+            "Khyber Pakhtunkhwa",
+            "Punjab",
+            "Sindh",
+            ])
+            .range(["#440154ff", "#21908dff", "#fde725ff", "#129490", "#CE1483"]);
+        
+        // JOIN data to elements.
+        let circles = svg.selectAll("circle").data(filteredData, function (d) {
+            return d["name"];
+        });
+        
+        let xAxis = 0;
+        // ENTER new elements present in new data.
+        circles
+            .enter()
+            .append("circle")
+            .attr("fill", function (d) {
+                return color(d["name"]);
+            })
+            .attr("cy", function (d) {
+                return y(d["earnings"]);
+            })
+            .attr("cx", function (d) {
+                xAxis++
+                return x(xAxis);
+            })
+            .attr("r", 5);
+    }
+    updateChart(games);
 })
