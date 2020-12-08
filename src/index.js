c4ecb2f2494639143a62dd77a4f31a5ef1bec3df
unknown <dannythanhphan@gmail.com>
2020-12-07 19:56:20 -0800

    Add data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+require("babel-polyfill");
+import data from '../public/check.json';
+
 document.addEventListener("DOMContentLoaded", () => {
-    const width = 900,
-        height = 600,
-        maxRadius = (Math.min(width, height) / 2) - 5;
-        
-    d3.json('../public/check.json').then(function(data) {
-        console.log(data);
-     })
-     .catch(function(error) {
-        console.log("error", error);
-     });
+    let margin = { top: 10, right: 30, bottom: 50, left: 60 },
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    function getHighestEarning(data) {
+        let max = 0;
+        let copy = [...data];
+        console.log(copy)
+        for (let i = 0; i < copy.length; i++) {
+            if (copy[i].earnings > max) max = copy[i].earnings;
+        }
+
+        return max;
+    }
+
+    let svg = d3.select("#dataset")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    
 })
